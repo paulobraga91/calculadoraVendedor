589f6e26d361f4195533f3ace738218402b7edc5
paulobraga.91@gmail.com <paulobraga.91@gmail.com>
2023-09-09 06:22:11 -0300

    alteracao da lógica

--- js.js
+++ js.js
@@ -6,7 +6,6 @@ const viewEsta = document.getElementById('viewEsta')
 const viewValor = document.getElementById('viewValor')
 const viewEstaText = document.querySelectorAll('.ViewEstaTexto')
 
-
 let func = document.getElementById('func')
 let funcEstagiario = document.getElementById('funcEstagiario')
 let valor = document.querySelector('.inputValor')
@@ -16,11 +15,11 @@ const resposta = document.querySelector('.hiden')
 btnConsultar.addEventListener('click',function(e){
    
 
-    let valorVenda = valor.value
+   
     let quantidadeFuncionarios = Number(func.value)
     let quantidadeEstagiario = Number(funcEstagiario.value)
-    let proporcaoFixo, proporcaoEstagiario
-    let valorSemMascara = removeMascara(valorVenda)
+    let proporcaoFixo = Number(0), proporcaoEstagiario = Number(0)
+    let valorSemMascara = removeMascara(valor.value)
     
 
     if(func.value == null || func.value == 0){
@@ -28,7 +27,7 @@ btnConsultar.addEventListener('click',function(e){
         return
     }
 
-    if(valorSemMascara == null||valorSemMascara == 0){
+    if(valorSemMascara === '' ||valorSemMascara == 0){
         alert('Digite o valor da meta do dia')
         return
     }
@@ -46,24 +45,20 @@ btnConsultar.addEventListener('click',function(e){
     }    
     
     if(ratio < .5){
-        proporcaoEstagiario = 10
-        proporcaoFixo = 90
+        proporcaoEstagiario = 20
+        proporcaoFixo = 80
     }else if(ratio >= .5 && ratio <= 1.4){
         proporcaoFixo = 80
         proporcaoEstagiario = 20
-    }else if(ratio > 1.5){
+    }else if(ratio >= 1.5){
         proporcaoEstagiario = 40
         proporcaoFixo = 60
     }
 
-
-    let valorFunc = (((valorSemMascara*proporcaoFixo)/100)/quantidadeFuncionarios).toFixed(2)
-    let valorEstagio = (((valorSemMascara*proporcaoEstagiario)/100)/quantidadeEstagiario).toFixed(2)
+    let valorFunc = (((valorSemMascara*proporcaoFixo)/100)/quantidadeFuncionarios)
+    let valorEstagio = (((valorSemMascara*proporcaoEstagiario)/100)/quantidadeEstagiario)
   
-    if(valorFunc == NaN || valorEstagio == NaN){
-        return
-    }
-    
+       
     viewValor.textContent = `R$ ${valorFunc}`
     viewEsta.textContent = `R$ ${valorEstagio}`
     viewFunc.textContent = `Estagiários(${quantidadeEstagiario}) + Fixos(${quantidadeFuncionarios}) = ${quantidadeEstagiario+quantidadeFuncionarios} `
@@ -71,10 +66,9 @@ btnConsultar.addEventListener('click',function(e){
     if (resposta.classList.contains('hiden')){
         resposta.classList.remove('hiden')
     }
-   
-
-
 })
+
+
 
 btnlimpar.addEventListener('click', function(){
     if (!resposta.classList.contains('hiden')){
@@ -84,11 +78,9 @@ btnlimpar.addEventListener('click', function(){
     func.value = ''
     funcEstagiario.value = ''
     
-
-
 })
 
-document.addEventListener('input',function(e){
+document.addEventListener('input',function(){
     let valorMascarado = valor.value
 
     valorMascarado = valorMascarado.replace(/\D/g, '')
@@ -99,7 +91,6 @@ document.addEventListener('input',function(e){
 })
 
 function removeMascara(valor){
-    
-    return parseFloat(valor.replace(/[^0-9,.]/g, '').replace(',','.'))
-
+    valor = parseFloat(valor.replace(/[^0-9,.]/g, '').replace(',','.'))
+    return Number(valor)
 }
